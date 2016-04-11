7aafcd46ef3cba33e313dba2f68c966e61d3f876
Nikita Schicalov <shicalov1994@gmail.com>
2016-04-11 14:35:24 +0300

    многомодульная структура

---
+++ service/src/main/java/service/actor/request2parser/RequestToParserActor.java
@@ -0,0 +1,27 @@
+package service.actor.request2parser;
+
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import domain.ParserResult;
+import service.actor.processingresult.ProcessingResultOfParserActor;
+import service.viewmodel.RequestParamsVewModel;
+
+
+/**
+ * Created by Никита on 05.04.2016.
+ */
+public class RequestToParserActor extends UntypedActor {
+
+    @Override
+    public void onReceive(Object msg) throws Exception {
+        if (msg instanceof RequestParamsVewModel) {
+            ActorRef processingResultOfParserActor = getContext().system().actorOf(Props.create(ProcessingResultOfParserActor.class), "processingResultOfParserActor");
+            //Закончен запрос к парсеру, вызываю актор обработки результата
+            System.out.println("Request2Parser");
+            processingResultOfParserActor.tell(new ParserResult(), getSelf());
+        } else
+            unhandled(msg);
+    }
+}
