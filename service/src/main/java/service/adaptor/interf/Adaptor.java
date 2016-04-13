115e453298e3756247998c215bb0f808c1273001
Nikita Batiushkov <nikita.batiushkov@waveoc.com>
2016-04-13 12:37:23 +0300

    Adaptor invoke after parser get result

--- service/src/main/java/service/adaptor/interf/Adaptor.java
+++ service/src/main/java/service/adaptor/interf/Adaptor.java
@@ -2,7 +2,10 @@ package service.adaptor.interf;
 
 import parser.PreparedData;
 import service.RequestData;
+import vo.view.IMTAward;
+
+import java.util.List;
 
 public interface Adaptor {
-    PreparedData adaptData(RequestData requestData);
+    List<IMTAward> adaptData(List<?> parserData);
 }
